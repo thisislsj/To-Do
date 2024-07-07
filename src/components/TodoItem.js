58fd54b0a3c553395e113da39ab08870dd9c2d8a
thisislsj <lahirusandeepa@outlook.com>
2024-07-07 12:32:04 +0530

    Implement component TodoItem

---
+++ src/components/TodoItem.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const ToDoItem = ({ todo, toggleToDo, deleteToDo }) => (
+    <div>
+        {/* Checkbox to toggle To Do completion status */}
+        <input
+            type="checkbox"
+            checked={todo.completed}
+            onChange={() => toggleToDo(todo.id)}
+        />
+        {/* Todo text with strike-through if completed */}
+        <span style={{textDecoration: todo.completed? 'line-through':'none'}}>
+            {todo.text}
+        </span>
+        {/* Button to delete the ToDo */}
+        <button onClick={()=>deleteToDo(todo.id)}>Delete</button>
+    </div>
+);
+
+ToDoItem.PropTypes = {
+    todo:PropTypes.object.isRequired,
+    toggleToDo:PropTypes.func.isRequired,
+    deleteToDo:PropTypes.func.isRequired,
+}
+
+export default ToDoItem;
